469f26f6ada9f70c957f931a6ae5ffc7ff76c7ee
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2023-05-13 11:52:47 +0200

    Add match or dislike functionality

--- Dog.js
+++ Dog.js
@@ -6,10 +6,10 @@ class Dog {
   getDogHtml() {
     const { name, avatar, age, bio } = this;
     return `
-    <div class="dog-container">
+    
         <img
           src="./images/badge-like.png"
-          alt="No badge"
+          alt="Like badge"
           class="like-badge-icon"
         />
         <img
@@ -26,23 +26,8 @@ class Dog {
           <h1 class="dog-name">${name}, ${age}</h1>
           <p class="dog-greeting">${bio}</p>
         </div>
-      </div>
-      <div class="buttons-container">
-        <div class="decline-button">
-          <img
-            src="./images/icon-cross.png"
-            alt="Cross icon"
-            class="button-icon"
-          />
-        </div>
-        <div class="like-button">
-          <img
-            src="./images/icon-heart.png"
-            alt="Heart icon"
-            class="button-icon"
-          />
-        </div>
-      </div>`;
+      
+      `;
   }
 }
 export default Dog;
--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 import dogs from './data.js';
 import Dog from './Dog.js';
 let dogsArray = ['Rex', 'Bella', 'Teddy'];
+document.getElementById('heart-button').addEventListener('click', like);
+document.getElementById('cross-button').addEventListener('click', decline);
+
 function getNewDog() {
   const nextDogData = dogs[dogsArray.shift()];
   return nextDogData ? new Dog(nextDogData) : {};
 }
 
-function render() {
-  document.getElementById('main-container').innerHTML = dog.getDogHtml();
+function like() {
+  document.querySelector('.like-badge-icon').style.display = 'inline-block';
+  dog.hasBeenLiked = true;
+  dog.hasBeenSwiped = true;
+  setTimeout(() => {
+    dog = getNewDog();
+    render();
+  }, 1500);
 }
+
+function decline() {
+  document.querySelector('.no-badge-icon').style.display = 'inline-block';
+  dog.hasBeenSwiped = true;
+  setTimeout(() => {
+    dog = getNewDog();
+    render();
+  }, 1500);
+}
+
+function render() {
+  document.getElementById('dog-container').innerHTML = dog.getDogHtml();
+}
+
 let dog = getNewDog();
 render();
